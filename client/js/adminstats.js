d7555182a1823c57240bc86ae62fefaa9e5fc8d7
VenkataNarayana Mogili <venkatanarayanamogili312@gmail.com>
2025-07-31 16:57:09 +0530

    adminstats.js

--- client/js/adminstats.js
+++ client/js/adminstats.js
@@ -5,7 +5,7 @@ window.onload = async function () {
     const token = localStorage.getItem("token");
     console.log("🔐 Loaded token:", token);
 
-    const res = await fetch("https://blood-bank-management-system-p5yt.onrender.com/api/adminstats", {
+    const res = await fetch("https://blood-bank-management-system-p5yt.onrender.com/api/stats", {
       method: "GET",
       headers: {
         "Authorization": `Bearer ${token}`,
